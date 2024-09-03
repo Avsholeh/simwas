719a5a3541e79649aeaf2ab966e2ecf2ec803a97
Muhammad Sholeh <avsholeh@gmail.com>
2024-09-03 21:09:54 +0700

    feat: update ui

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -9,6 +9,7 @@ use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
+use Filament\Support\Enums\MaxWidth;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -58,6 +59,8 @@ class AppPanelProvider extends PanelProvider
                 'Manajemen Tim',
                 'Pengaturan',
             ])
-            ->databaseTransactions();
+            ->databaseTransactions()
+            ->sidebarCollapsibleOnDesktop()
+            ->maxContentWidth(MaxWidth::Full);
     }
 }
